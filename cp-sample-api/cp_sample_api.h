8bd6730004ff45999f831d3e1c4c7871628ff46b
Danilo Gomes <dgomes@accela.com>
2018-03-07 14:57:10 +0000

    Initial Commit

---
+++ cp-sample-api/cp_sample_api.h
@@ -0,0 +1,19 @@
+//
+//  cp_sample_api.h
+//  cp-sample-api
+//
+//  Created by Danilo Gomes on 3/7/18.
+//  Copyright © 2018 Accela Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for cp_sample_api.
+FOUNDATION_EXPORT double cp_sample_apiVersionNumber;
+
+//! Project version string for cp_sample_api.
+FOUNDATION_EXPORT const unsigned char cp_sample_apiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cp_sample_api/PublicHeader.h>
+
+
